7d26759b2a08c2bba157dd8bd1d2887369980024
abed-961 <abdallahkhoder59@gmail.com>
2024-10-03 12:49:18 +0300

    Master (#1)
    * m1
    
    * m2
    
    * m3
    
    * b1-m1
    
    * m4
    
    * b2-m1
    
    * b1-m2
    
    * merge b1 to main  part 2
    
    * b2-m2
    
    * adding b2-m1 after m2
    
    ---------
    
    Co-authored-by: abdallah <abdkho691@gmail.com>

---
+++ exam.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 3, 4, 5];
+
+function click() {
+	window.location.href = "www.google.com";
+}
+
+console.log("this is the m4 in main branch ");
+
+console.log("we creating b2 after commit m4 ");
+console.log("here is the first commit in b1 branch ");
+
+const b1 = "b1-m2 commit ";
+
+const b2M2 = "this is b2-m2 commmit ";
+
+const b2M1 = "I forget to add the first commit in b2";
