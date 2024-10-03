c6db0f2a2ea5deeddd97ee02e580851b7f61ee13
abdallah <abdkho691@gmail.com>
2024-10-03 12:35:58 +0300

    merge b1 to main  part 2

--- exam.js
+++ exam.js
@@ -4,11 +4,8 @@ function click() {
 	window.location.href = "www.google.com";
 }
 
-<<<<<<< HEAD
 console.log("this is the m4 in main branch ");
 console.log("we creating b2 after commit m4 ");
-=======
 console.log("here is the first commit in b1 branch ");
 
 const b1 = "b1-m2 commit ";
->>>>>>> b1
